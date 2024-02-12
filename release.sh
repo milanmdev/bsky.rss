3bfee83b6710b96530f829d4713ebd4a73bd317d
milanmdev <milanmdev@gmail.com>
2024-02-11 21:54:03 -0600

    fix release script

--- release.sh
+++ release.sh
@@ -15,19 +15,21 @@ fi
 versionInit=$(cat ./package.json | yarn run underscore select '.version')
 versionNoQuote=${versionInit//\"/}
 version=$(echo $versionNoQuote | sed 's/[][]//g')
+branch="$(git rev-parse --abbrev-ref HEAD)"
 
 # Removing old images
 echo "Removing old images..."
 docker images | grep bsky.rss | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi ghcr.io/milanmdev/bsky.rss:{} --force
 
-if [[ $(echo git rev-parse --abbrev-ref HEAD) != "main" ]]
-then
+if [[ "$branch" != "main" ]]; then
     echo "Building \"$(git rev-parse --abbrev-ref HEAD)\" branch image..."
-    docker build . --platform linux/x86_64 -t ghcr.io/milanmdev/bsky.rss:latest #$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+    docker build . --platform linux/x86_64 -t ghcr.io/milanmdev/bsky.rss:$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+
+    echo "main"
 
     # Push the latest image
     echo "Pushing the image..."
-    docker push ghcr.io/milanmdev/bsky.rss:latest #$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+    docker push ghcr.io/milanmdev/bsky.rss:$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 else
     echo "Building \"main\" branch image..."
     docker build . --platform linux/x86_64 -t ghcr.io/milanmdev/bsky.rss:latest -t ghcr.io/milanmdev/bsky.rss:$version
